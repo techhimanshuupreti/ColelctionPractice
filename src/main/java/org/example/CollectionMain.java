07ed4c61a424d4e97c800727c847826de058d40e
upreti <upretihimanshu16@gmail.com>
2023-03-05 13:28:10 +0530

    testing some method

--- src/main/java/org/example/CollectionMain.java
+++ src/main/java/org/example/CollectionMain.java
@@ -6,24 +6,23 @@ import java.util.concurrent.CopyOnWriteArrayList;
 public class CollectionMain {
   public static void main(String[] args) {
     System.out.println("Hello world!");
-//    List<String> stringList = new ArrayList<>();
-//    stringList.add("JAI");
-//    stringList.add("Karan");
-//    stringList.add("JAI");
-//    stringList.add("Amit");
-//    System.out.println("Size = " + stringList.size());
-//    List<String> stringList1 = new ArrayList<>();
-//    stringList1.add("Uttam");
-//    stringList1.add("Kamlesh");
-
-//    synchronize ArrayList 1. Collection.synchronizedList() - Method - return synchronized list
-    List<String> stringList = Collections.synchronizedList(new ArrayList<>());
+    List<String> stringList = new ArrayList<>();
     stringList.add("JAI");
     stringList.add("Karan");
     stringList.add("JAI");
     stringList.add("Amit");
+    System.out.println("Size = " + stringList.size());
+    List<String> stringList1 = new ArrayList<>();
+    stringList1.add("Umesh");
+    stringList1.add("Krishna");
 
+//    synchronize ArrayList 1. Collection.synchronizedList() - Method - return synchronized list
+    List<String> synchronizedStringList = Collections.synchronizedList(new ArrayList<>());
     //add, remove - we do not need explicit  synchronization
+    synchronizedStringList.add("JAI");
+    synchronizedStringList.add("Karan");
+    synchronizedStringList.add("JAI");
+    synchronizedStringList.add("Amit");
 
     // to fetch/ traverse the values from this list -- we have to use explicit synchronization
     synchronized (stringList){
@@ -33,73 +32,70 @@ public class CollectionMain {
       }
     }
 
-
 //    synchronize ArrayList 2. copyOnWriteArrayList - class - Thread Safe variant of ArrayList
     // Thread-safe/synchronized already. ( At a time only one thread will work on this list)
-    CopyOnWriteArrayList<String> empList = new CopyOnWriteArrayList<>();
-    empList.add("Tom");
-    empList.add("Steve");
-    empList.add("Naveen");
+    CopyOnWriteArrayList<String> stringCopyOnWriteArrayList = new CopyOnWriteArrayList<>();
+    stringCopyOnWriteArrayList.add("Tom");
+    stringCopyOnWriteArrayList.add("Steve");
+    stringCopyOnWriteArrayList.add("Naveen");
 
     // no need to explicit synchronization for any operation: add/remove/traverse
-    Iterator<String> iterator2 = empList.iterator();
+    Iterator<String> iterator2 = stringCopyOnWriteArrayList.iterator();
     while(iterator2.hasNext()){
       System.out.println(iterator2.next());
     }
 
-
-
 //=======================================================
-//    List<String> al = new ArrayList<>();
-//    al.add("jai");
-//    al.add("karan");
-//    al.add("amit");
+    List<String> al = new ArrayList<>();
+    al.add("jai");
+    al.add("karan");
+    al.add("amit");
 
     System.out.println("----------");
-//    stringList1.addAll(stringList);
-//    stringList1.add(1,"add_in_1");
-//    stringList1.addAll(1,stringList);
-//    stringList1.stream().forEach(ele->System.out.println(ele));
+    stringList1.addAll(stringList);
+    stringList1.add(1,"add_in_1");
+    stringList1.addAll(1,stringList);
+    stringList1.stream().forEach(ele->System.out.println(ele));
 
-//    System.out.println("Test contains = "+stringList.contains("jai"));
-//    System.out.println("1----------");
-//    stringList.stream().forEach(ele->System.out.println(ele));
-//    System.out.println("Test containsAll = "+stringList.containsAll(al));
-//    System.out.println("2----------");
-//    stringList.stream().forEach(ele->System.out.println(ele));
-//    System.out.println("Test index = ");
-//    System.out.println(stringList.indexOf("jai")>0); // check contains value
-//    System.out.println("3----------");
-//    stringList.stream().forEach(ele->System.out.println(ele));
-//
-//    System.out.println("4----------");
-//    stringList.stream().forEach(ele->System.out.println(ele));
-//    System.out.println(stringList.lastIndexOf("JAI")); // get index value of last repeated value from starting values
-//    System.out.println("5----------");
-//    stringList.stream().forEach(ele->System.out.println(ele));
-//    System.out.println("6----------");
-//    stringList.stream().forEach(ele->System.out.println(ele));
-//    System.out.println(stringList.remove("Jai")); // remove by value
-//    System.out.println("7----------");
-//    stringList.stream().forEach(ele->System.out.println(ele));
-//    System.out.println(stringList.remove(1)); // remove by index
-//    System.out.println("8----------");
-//    stringList.stream().forEach(ele->System.out.println(ele));
-//    System.out.println(stringList.removeIf(num-> num.startsWith("A"))); // remove by index
-//    System.out.println("9----------");
-//    stringList.stream().forEach(ele->System.out.println(ele));
+    System.out.println("Test contains = "+stringList.contains("jai"));
+    System.out.println("1----------");
+    stringList.stream().forEach(ele->System.out.println(ele));
+    System.out.println("Test containsAll = "+stringList.containsAll(al));
+    System.out.println("2----------");
+    stringList.stream().forEach(ele->System.out.println(ele));
+    System.out.println("Test index = ");
+    System.out.println(stringList.indexOf("jai")>0); // check contains value
+    System.out.println("3----------");
+    stringList.stream().forEach(ele->System.out.println(ele));
 
-//    stringList.retainAll(Collections.singleton("JAI")); // only match value will be return
-//    System.out.println("10----------");
-//    stringList.stream().forEach(ele->System.out.println(ele));
+    System.out.println("4----------");
+    stringList.stream().forEach(ele->System.out.println(ele));
+    System.out.println(stringList.lastIndexOf("JAI")); // get index value of last repeated value from starting values
+    System.out.println("5----------");
+    stringList.stream().forEach(ele->System.out.println(ele));
+    System.out.println("6----------");
+    stringList.stream().forEach(ele->System.out.println(ele));
+    System.out.println(stringList.remove("Jai")); // remove by value
+    System.out.println("7----------");
+    stringList.stream().forEach(ele->System.out.println(ele));
+    System.out.println(stringList.remove(1)); // remove by index
+    System.out.println("8----------");
+    stringList.stream().forEach(ele->System.out.println(ele));
+    System.out.println(stringList.removeIf(num-> num.startsWith("A"))); // remove by index
+    System.out.println("9----------");
+    stringList.stream().forEach(ele->System.out.println(ele));
 
-//    ArrayList<Integer> number1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13));
-//    System.out.println(number1);
-//    ArrayList<Integer> subNumber1 = new ArrayList<>(number1.subList(1,6));
-//    System.out.println(subNumber1);
+    stringList.retainAll(Collections.singleton("JAI")); // only match value will be return
+    System.out.println("10----------");
+    stringList.stream().forEach(ele->System.out.println(ele));
 
-//    ArrayList<Integer> newList = new ArrayList<>(number1.subList(2,6));
-//    System.out.println(number1);
+    ArrayList<Integer> number1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13));
+    System.out.println(number1);
+    ArrayList<Integer> subNumber1 = new ArrayList<>(number1.subList(1,6));
+    System.out.println(subNumber1);
+
+    ArrayList<Integer> newList = new ArrayList<>(number1.subList(2,6));
+    System.out.println(number1);
 
 
 //    System.out.println("----------");
--- src/main/java/org/example/RemoveDuplicateElements.java
+++ src/main/java/org/example/RemoveDuplicateElements.java
@@ -13,17 +13,21 @@ public class RemoveDuplicateElements {
             (Arrays.asList(1,2,3,4,5,1,1,4,3,2,5,6,7,7,8,9));
     System.out.println(numbers);
 //    1. LinkedHashSet
+    System.out.println("Starting LinkedHashSet and Time "+ System.currentTimeMillis());
     LinkedHashSet<Integer> linkedHashSet = new LinkedHashSet<Integer>(numbers);
-
+    System.out.println("end LinkedHashSet and Time "+ System.currentTimeMillis());
     ArrayList<Integer> duplicateNumberRemove = new ArrayList<>(linkedHashSet);
+    System.out.println("end ArrayList and Time "+ System.currentTimeMillis()); // 16 millis second
     System.out.println(duplicateNumberRemove);
 
 //    2. JDK 1.8 - Stream
+    System.out.println("Starting Stream and Time "+ System.currentTimeMillis());
     ArrayList<Integer> markNumber = new ArrayList<>
             (Arrays.asList(1,2,3,4,5,1,1,4,3,2,5,6,7,7,8,9));
     System.out.println(numbers);
     List<Integer> duplicateNumberRemoveList =  markNumber.stream().distinct().collect(Collectors.toList());
     System.out.println(duplicateNumberRemoveList);
+    System.out.println("Starting Stream and Time "+ System.currentTimeMillis());
 
 
 
